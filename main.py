ebd49092603758a3ba9d1e2dd079c753d55adeea
mxyz123 <matus.sirotek@gmail.com>
2023-10-24 17:47:10 +0200

    Added bootstrap and login,register

--- main.py
+++ main.py
@@ -29,6 +29,14 @@ def anj():
 def feit():
     return render_template('feit.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
